25dfb28799b8fae39e3236b1d71bd9451cfb60be
Felipe Lima <flimas1985@gmail.com>
2021-08-04 00:18:55 -0300

    Primeiro commit: Servidor e primeiras classes servlets criadas.

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,29 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi mundo, parabens vc escreveu o primeiro servlet.");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("o servlet Oi mundo foi chamado");
+		
+	}
+
+}
